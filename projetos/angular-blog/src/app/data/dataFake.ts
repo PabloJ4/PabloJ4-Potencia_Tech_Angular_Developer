a592c63dcc57de854eae34814912c9dfd61de2fc
PabloJ4 <008pablo008@gmail.com>
2023-05-22 19:32:22 -0300

    content created

--- projetos/angular-blog/src/app/data/dataFake.ts
+++ projetos/angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
     {
-        "id":1,
-        "photo":"aaa",
-        "title":"saiu novo filme",
-        "description":"ola"
+        "id":"1",
+        "title":"Novo filme dos Vingadores",
+        "description":"O filme vai se passar np universo dos X-man onde ambos vão lutar contra o Hulk descontrolado",
+        "photoCover" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB90wQ15WrjnHGwZevVQPcNn_V7KklCB4pKw&usqp=CAU"
     },
     {
-        "id":2,
-        "photo":"aaa",
-        "title":"saiu novo filme",
-        "description":"ola"
+        "id":"2",
+        "title":"Os novo parceiros do Homem Formiga",
+        "description":"ola",
+        "photoCover" : "https://f.i.uol.com.br/fotografia/2023/02/16/167658634563eead69d6a11_1676586345_3x2_md.jpg"
     }
 ]
--- projetos/angular-blog/src/app/pages/content/content.component.ts
+++ projetos/angular-blog/src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -12,10 +14,26 @@ export class ContentComponent implements OnInit {
   contentTitle: string="";
   @Input()
   contentDescription: string="";
+  private id: string | null = "0"
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
+      )
+
+      this.setvaluesToComponent(this.id)
+  }
+  setvaluesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id === id)[0]
+
+    this.contentTitle =result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+
   }
 
 }

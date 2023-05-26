bcb34692642b969e24875530f6a3f251a0041911
PabloJ4 <008pablo008@gmail.com>
2023-05-26 20:30:47 -0300

    inicio do clone do buzzfeed

---
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+export class QuizzComponent implements OnInit {
+
+  title: string = "";
+  questions: any
+  questionsSelected: any
+
+  answers: string[] = [];
+  answersSelected: string =""
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0; 
+
+  finished: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
